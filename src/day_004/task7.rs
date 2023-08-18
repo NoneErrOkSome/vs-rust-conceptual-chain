7307813626f0caf05c3548d32bd5914ab668c2d7
Suramethar Chantarasmee <hlek@live.com>
2023-08-18 21:16:39 +0700

    a

--- src/day_004/task7.rs
+++ src/day_004/task7.rs
@@ -1,7 +1,14 @@
 
+// 🎯 Task 7: Iterating Over a Vector with for Loop
+// Create a vector of strings representing a list of tasks. 
+// Iterate over the vector using a for loop and print each task.
 
 pub fn task47() {
     if !cfg!(feature = "task47") {
-        
+        let vec: Vec<i32> = vec![1,2,3,4,5];
+        for i in &vec {
+            println!("{}", i);
+        }
+
     }
 }
