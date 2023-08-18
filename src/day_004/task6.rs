2e5dea2775738194b798977403bc30a5ee3940b1
Suramethar Chantarasmee <hlek@live.com>
2023-08-18 21:09:59 +0700

    a

--- src/day_004/task6.rs
+++ src/day_004/task6.rs
@@ -1,6 +1,17 @@
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
 
 pub fn task46() {
     if !cfg!(feature = "task46") {return;}
 
-    println!("This is from day_004 function!");
+    let n = 10;
+    println!("Fibonacci {} = {}", n, fibonacci(n));
+
 }
