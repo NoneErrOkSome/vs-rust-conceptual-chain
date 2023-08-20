bdb025dca37bfe8798a533adeab87dccb7c4ce19
Suramethar Chantarasmee <hlek@live.com>
2023-08-20 19:02:10 +0700

    a

--- src/day_005/task0.rs
+++ src/day_005/task0.rs
@@ -4,5 +4,17 @@ pub fn task50() {
 if !cfg!(feature = "task50") {return;}
 
 
+let mut n = 3;
+    print!("{} ", n);
+    while n != 1 {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        print!("{} ", n);
+    }
+    println!();
+
 
 }
