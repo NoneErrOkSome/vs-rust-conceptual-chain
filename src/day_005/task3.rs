9b6362dc01c6f771ba756ddfbf1c01f828490617
Suramethar Chantarasmee <hlek@live.com>
2023-08-21 16:28:03 +0700

    a

--- src/day_005/task3.rs
+++ src/day_005/task3.rs
@@ -1,5 +1,18 @@
+fn length(s: String) -> u32 {
+    s.len().try_into().unwrap()
+}
+
+fn borrowed(str: &String) {
+    println!("{}", str);
+}
 
 pub fn task53() {
+    if !cfg!(feature = "task53") {return;}
+    let str = String::from("Hello");
+    let length = length(str);
+    println!("{}", length);
 
+    let borrow = "You've borrowed".to_string();
+    borrowed(&borrow);
     
 }
--- src/day_005/task4.rs
+++ src/day_005/task4.rs
@@ -1,5 +1,18 @@
+fn longer<'a>(s1: &'a str, s2: &'a str) {
+
+    if s1.len() > s2.len() {
+        println!("s1 has {} letter, s2 has {} letter", s1.len(), s2.len());
+    } else {
+        println!("s2 has {} letter, s1 has {} letter", s2.len(), s1.len());
+    }
+}
+
 
 pub fn task54() {
+    if !cfg!(feature = "task54") {return;}
 
-    
+    let a = "haha";
+    let b = "ha";
+    longer(a, b);
+
 }
