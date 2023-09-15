5fcd61e8f1127e2712a8c289dcaa45318f4c443f
Suramethar Chantarasmee <hlek@live.com>
2023-09-15 17:22:47 +0700

    a

--- src/day_008/task0.rs
+++ src/day_008/task0.rs
@@ -1,5 +1,6 @@
 pub fn task80() {
-    if !cfg!(feature = "task80") {return;}
-    println!("task80");
+        if !cfg!(feature = "task80") {return;}
+        // Generate LaTeX for quadratic equation
 
 }
+    
